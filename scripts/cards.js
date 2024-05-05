0f70dc41b4e1c421b213aa22d3ee2f6c38b5129e
Stacey Kremer <staceybumblebee@gmail.com>
2024-05-05 17:34:34 +0500

    fixation

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Данакиль",
+      link: "https://images.unsplash.com/photo-1603456771349-07ea0241821f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Парк Хитачи",
+      link: "https://images.unsplash.com/photo-1687355948708-54dfff9d488e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Гранд-Каньон",
+      link: "https://images.unsplash.com/photo-1571761871155-65379e0ffa1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Памуккале",
+      link: "https://plus.unsplash.com/premium_photo-1669865741968-f8578f8e597e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Озеро Брайес",
+      link: "https://images.unsplash.com/photo-1671904597069-00b075ab278a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Сад Богов",
+      link: "https://images.unsplash.com/photo-1597413134301-80646fc15e9d?q=80&w=1767&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const cardItem = document.querySelector('.places__item card');
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function addCard(name, link, deleteCard) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__title').textContent = name;
+	cardElement.querySelector('.card__image').src = link;
+	cardElement.querySelector('.card__image').alt = name;
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', () => deleteCard(deleteButton));
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(deleteButton) {
+	const cardItemDelete = deleteButton.closest('.card');
+	cardItemDelete.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+	cardList.append(addCard(item.name, item.link, deleteCard));
+});
